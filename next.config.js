7b451f3e00a7f1a8d87afda897ed08b0a803f111
felirami <felirami@users.noreply.github.com>
2025-11-08 08:50:32 -0300

    Fix font CSP: allow all HTTPS fonts and WalletConnect styles
    - Change font-src to allow all HTTPS fonts (https:)
    - Add WalletConnect domains to style-src
    - Fixes font loading errors from WalletConnect/Reown components

--- next.config.js
+++ next.config.js
@@ -11,8 +11,8 @@ const nextConfig = {
             value: [
               "default-src 'self'",
               "script-src 'self' 'unsafe-eval' 'unsafe-inline' https://*.walletconnect.com https://*.walletconnect.org https://*.web3modal.org",
-              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com",
-              "font-src 'self' https://fonts.gstatic.com https://fonts.googleapis.com data: blob:",
+              "style-src 'self' 'unsafe-inline' https://fonts.googleapis.com https://*.walletconnect.com https://*.walletconnect.org",
+              "font-src 'self' https: data: blob:",
               "img-src 'self' data: https: blob:",
               "connect-src 'self' https://*.walletconnect.com https://*.walletconnect.org https://*.reown.com https://*.web3modal.org https://api.web3modal.org https://cca-lite.coinbase.com wss://*.walletconnect.com wss://*.walletconnect.org",
               "frame-src 'self' https://*.walletconnect.com https://*.walletconnect.org https://secure.walletconnect.org",
